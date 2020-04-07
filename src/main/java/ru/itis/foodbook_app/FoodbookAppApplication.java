badf3120dafb97276e589d9ff665d5c571b8c45d
dariacherbaeva <dashacherbaeva@mail.ru>
2020-04-07 13:59:06 +0300

    localization started

--- src/main/java/ru/itis/foodbook_app/FoodbookAppApplication.java
+++ src/main/java/ru/itis/foodbook_app/FoodbookAppApplication.java
@@ -2,7 +2,9 @@ package ru.itis.foodbook_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,9 +19,9 @@ import java.util.concurrent.*;
 @SpringBootApplication
 public class FoodbookAppApplication {
 
-   // @Bean
+    // @Bean
     //public JavaMailSender mailSender() { return new JavaMailSenderImpl();
-   // }
+    // }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -32,7 +34,8 @@ public class FoodbookAppApplication {
     }
 
     @Bean
-    public RecipeService recipeService() {return new RecipeServiceImpl();
+    public RecipeService recipeService() {
+        return new RecipeServiceImpl();
     }
 
 
